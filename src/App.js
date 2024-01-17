97a1a14fc03f195afc07be33f231bf9f81a95522
DabeenJung <dabeenj24@gmail.com>
2024-01-16 20:13:00 -0800

    created navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
 import "./index.css";
+import Home from "./routes/Home";
+import About from "./routes/About";
+import Contact from "./routes/Contact";
+import Project from "./routes/Project";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      This is app.js
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/project" element={<Project />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </>
   );
 }
 
