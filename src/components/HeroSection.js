2bda2a9ae05a6378f039fdbfb72b716237e3309b
DabeenJung <dabeenj24@gmail.com>
2024-01-16 20:57:20 -0800

    working on the hero section

---
+++ src/components/HeroSection.js
@@ -0,0 +1,29 @@
+import "./HeroSectionStyles.css";
+
+import React from 'react'
+
+const HeroSection = () => {
+  return (
+    <section id="heroSection" className="hero--section">
+      <div className="hero--section--content--box">
+        <div className="hero--section--content">
+          <p className="section--title">
+            Hey, I'm Ellen!
+          </p>
+          <h1 className="hero--section--title">
+            <span className="hero--section--title--color">Full
+            Stack</span>{" "}
+            <br />
+            Developer
+          </h1>
+        </div>
+        <button className="btn btn-primary">Get In Touch</button>
+      </div>
+      <div className="hero--section--img">
+        <img src="./img/hero_img.png" alt="Hero Section" />
+      </div>
+    </section>
+  )
+}
+
+export default HeroSection
